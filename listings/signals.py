cadb8f6865b6e881d5c49a9e1fb342a2b81c0f64
Rnaji <145147110+Rnaji@users.noreply.github.com>
2024-01-27 21:20:49 +0100

    about

--- listings/signals.py
+++ listings/signals.py
@@ -3,43 +3,60 @@
 from django.db.models.signals import post_delete, pre_save, post_save
 from django.dispatch import receiver
 from .models import UniqueReference, RepairStore
+import logging
+
+logger = logging.getLogger(__name__)
 
 @receiver(post_delete, sender=UniqueReference)
 def create_new_unique_reference_after_delete(sender, instance, **kwargs):
-    """
-    Crée une nouvelle référence unique lorsque l'ancienne est supprimée,
-    à condition que le nombre total de références non utilisées soit inférieur à 65.
-    """
-    current_references_count = UniqueReference.objects.filter(repairstore=instance.repairstore, is_used=False).count()
+    if instance.repairstore:
+        try:
+            current_references_count = UniqueReference.objects.filter(repairstore=instance.repairstore, is_used=False).count()
 
-    if current_references_count < 65:
-        create_new_unique_reference(instance.repairstore)
+            if current_references_count < 65:
+                create_new_unique_reference(instance.repairstore)
+
+        except Exception as e:
+            logger.error(f"Error in create_new_unique_reference_after_delete: {e}", exc_info=True)
+
 
 @receiver(pre_save, sender=UniqueReference)
 def create_new_unique_reference_before_save(sender, instance, **kwargs):
-    """
-    Crée une nouvelle référence unique avant la sauvegarde si is_used est défini à True.
-    """
-    if instance.is_used:
-        create_new_unique_reference(instance.repairstore)
+    if instance.is_used and instance.repairstore:
+        try:
+            create_new_unique_reference(instance.repairstore)
+
+        except Exception as e:
+            logger.error(f"Error in create_new_unique_reference_before_save: {e}", exc_info=True)
+
 
 def create_new_unique_reference(repairstore):
     """
     Fonction utilitaire pour créer une nouvelle référence unique non utilisée.
     """
-    while True:
-        new_reference_value = UniqueReference.generate_unique_reference_value()
+    if repairstore:
+        while True:
+            new_reference_value = UniqueReference.generate_unique_reference_value()
 
-        # Vérifier si la nouvelle référence n'est pas déjà attribuée à un utilisateur
-        if not UniqueReference.objects.filter(value=new_reference_value).exists():
-            UniqueReference.objects.create(repairstore=repairstore, value=new_reference_value)
-            break
+            # Vérifier si la nouvelle référence n'est pas déjà attribuée à un utilisateur
+            if not UniqueReference.objects.filter(value=new_reference_value).exists():
+                UniqueReference.objects.create(repairstore=repairstore, value=new_reference_value)
+                break
+
 
 @receiver(post_save, sender=RepairStore)
 def ensure_user_has_65_unique_references(sender, instance, created, **kwargs):
-    if created:  # Vérifier si l'instance est nouvellement créée
-        current_references_count = UniqueReference.objects.filter(repairstore=instance, is_used=False).count()
+    if created and instance:  # Vérifiez si l'instance est nouvellement créée et non nulle
+        try:
+            current_references_count = UniqueReference.objects.filter(repairstore=instance, is_used=False).count()
 
-        if current_references_count < 65:
-            for _ in range(65 - current_references_count):
-                create_new_unique_reference(instance)
+            if current_references_count < 65:
+                new_references = [
+                    UniqueReference(repairstore=instance, value=UniqueReference.generate_unique_reference_value())
+                    for _ in range(65 - current_references_count)
+                ]
+                UniqueReference.objects.bulk_create(new_references)
+
+        except Exception as e:
+            logger.error(f"Error in ensure_user_has_65_unique_references: {e}", exc_info=True)
+
